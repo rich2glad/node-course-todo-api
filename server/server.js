d4025d0e0a713486ce8f692f2a43e8680ef0f93e
rich2glad <rich2glad@gmail.com>
2018-09-10 15:57:19 -0400

    GET:id route

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {monsooge}= require('./db/mongoose');
-
+const {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -31,6 +31,23 @@ app.get('/todos',(req,res)=>{
             res.status(400).send(e);
         })
     
+});
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+     res.status(404).send('ID is not valid');
+}
+    Todo.findById(id).then((todo)=>{
+        if(!todo)
+            res.status(404).send('record is not present');
+        
+           res.send({todo})
+        },(e)=>{
+        res.status(404).send(e);
+    })
+
 });
 
 app.listen(3000, ()=>{
