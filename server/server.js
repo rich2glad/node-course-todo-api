87ac58a160a86ffd7232e8f8355dfab9c7b7aca3
rich2glad <rich2glad@gmail.com>
2018-09-17 13:32:27 -0400

    delete route

--- server/server.js
+++ server/server.js
@@ -131,6 +131,14 @@ app.post('/users/login',(req,res)=>{
     });
 });
 
+app.delete('/users/me/delete',authenticate,(req,res)=>{
+   req.user.removeToken(req.token).then(()=>{
+       res.status(200).send()
+   },()=>{
+    res.status(400).send()
+   });
+});
+
 app.listen(port, ()=>{
     console.log(`started on port ${port}`);
 });
